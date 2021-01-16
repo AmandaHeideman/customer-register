584cb8b6322ab41c35fa78fea93568a1c327442b
Amanda Heideman <heideman.amanda@gmail.com>
2021-01-16 23:08:29 +0100

    add functionality to customer create page

--- src/pages/CustomerCreatePage.jsx
+++ src/pages/CustomerCreatePage.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useHistory} from 'react-router-dom'
 
 export default function CustomerCreatePage() {
+  const [formData, setFormData] = useState({})
+  const history = useHistory()
+
+  function handleOnChange(e) {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  function renderInput(name, label, type) {
+    return (
+      <div>
+        <label>{label}</label>
+        <input 
+          type={type || "text"} 
+          name={name} 
+          onChange={handleOnChange}
+        />
+      </div>
+    )
+  }
+
+  function handleOnSubmit(e){
+    e.preventDefault()
+    const url = "https://frebi.willandskill.eu/api/v1/customers/"
+    const token = localStorage.getItem("Amanda")
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      history.push('/customers')
+    })
+  }
+
   return (
     <div>
-      Create Customer
+      <h1>Create Customer</h1>
+      <form onSubmit={handleOnSubmit}>
+        {renderInput("name", "Customer Name")}
+        {renderInput("email", "Customer Email", "email")}
+        {renderInput("organisationNr", "Organisation Number")}
+        {renderInput("paymentTerm", "Payment Term", "number")}
+        {renderInput("phoneNumber", "Phone Number", "tel")}
+        {renderInput("reference", "Reference")}
+        {renderInput("vatNr", "Vat Number")}
+        {renderInput("website", "Website", "url")}
+        <button type="submit">Create Customer</button>
+      </form>
     </div>
   )
 }
