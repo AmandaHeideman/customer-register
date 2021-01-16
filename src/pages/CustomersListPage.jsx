e30353b4d86401587422b369fd8ac65c38f1afe9
Amanda Heideman <heideman.amanda@gmail.com>
2021-01-16 23:08:01 +0100

    add functionality to customer list page

--- src/pages/CustomersListPage.jsx
+++ src/pages/CustomersListPage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 
-export default function CustomersListPage() {
+export default function CustomerListPage() {
+  const [customerList, setCustomerList] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    getCustomerList();
+  }, []);
+
+  function getCustomerList() {
+    const url = "https://frebi.willandskill.eu/api/v1/customers/";
+    const token = localStorage.getItem("Amanda");
+    fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setCustomerList(data.results));
+  }
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    history.push("/customers/create");
+  }
   return (
     <div>
-      Customers List Page
+      {customerList[0] ? (
+        customerList.map((item) => {
+          return (
+            <h3>
+              <Link to={`/customers/${item.id}`}>{item.name}</Link>
+            </h3>
+          );
+        })
+      ) : (
+        <form onSubmit={handleOnSubmit}>
+          <p>No existing customers</p>
+          <button type="submit">Create customer</button>
+        </form>
+      )}
     </div>
-  )
+  );
 }
