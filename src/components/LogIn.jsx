b316a8e504f5ea0c8cf70df7ae164369034f656c
Amanda Heideman <heideman.amanda@gmail.com>
2021-01-16 19:30:33 +0100

    add functionality to log in

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
 
 export default function LogIn() {
+  const [formInput, setFormInput] = useState({
+    email: "Amanda.Heideman@yh.nackademin.se",
+    password: "javascriptoramverk"
+  })
+
+  const history = useHistory();
+
+  function handleOnChange(e){
+    e.preventDefault();
+    setFormInput({...formInput, [e.target.email]: e.target.password })
+  }
+
+  function handleOnSubmit(e){
+    e.preventDefault()
+    const url = "https://frebi.willandskill.eu/api-token-auth/"
+    const payload = {
+      email: formInput.email,
+      password: formInput.password
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      localStorage.setItem("WEBB20", data.token)
+      history.push('/customers')
+    })
+  }
+
   return (
     <div>
       <p>Log In</p>
-      <label>Username:
-        <input type="text" placeholder="Amanda.Heideman@yh.nackademin.se"/>
-      </label>
-      <label>Password:
-        <input type="password" placeholder="javascriptoramverk"/>
-      </label>
-      <button>Log in</button>
+      <form onSubmit={handleOnSubmit}>
+        <label>Email:
+          <input type="text" onChange={handleOnChange} value={formInput.email} />
+        </label>
+        <label>Password:
+          <input type="password" onChange={handleOnChange} value= {formInput.password} />
+        </label>
+        <button type="submit">Log in</button>
+      </form>
     </div>
   )
 }
